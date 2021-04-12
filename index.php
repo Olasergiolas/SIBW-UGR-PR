f5c832d66884bba743c00fe530392047b0d27e04
Sergio García <Olasergiolas@gmail.com>
2021-04-12 20:24:51 +0200

    Página principal ahora usa BD

--- index.php
+++ index.php
@@ -7,15 +7,7 @@
 
   $mysqli = conectarBD();
 
-  $eventos = [
-    'info' => [
-      'info_fila1' => ['Cyber Apocalypse 2021', 'PlaidCTF 2021', 'LINE CTF 2021'],
-      'info_fila2' => ['DEFCON', 'RSA Conference', 'Hack in Paris'],
-      'info_fila3' => ['Black Hat USA', 'Google CTF', 'NULLCON'],
-    ],
-  ];
-
-  //$eventos = getEventosBriefing($mysqli);
+  $eventos = getEventosBriefing($mysqli);
 
   echo $twig->render('index.html', ['listaeventos' => $eventos]);
 ?>
