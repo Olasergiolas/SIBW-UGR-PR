7bddf20c8cda181b2339d8a5e4219f58573687a6
Sergio García <Olasergiolas@gmail.com>
2021-05-28 20:25:56 +0200

    Barra de búsqueda de eventos terminada

--- JS/ajax.js
+++ JS/ajax.js
@@ -1,34 +1,29 @@
 $(document).ready(function() {
     $("#barra_busqueda_principal").keyup(hacerPeticionAjax);
-  });
+});
   
-  function hacerPeticionAjax() {
-      busqueda = $("#barra_busqueda_principal").val();
-      
-      $.ajax({
-        data: {busqueda:busqueda},
-        url: 'ajax.php',
-        method: 'POST',
-        success: procesaRespuestaAjax
-      });
-  }
-  
-  function procesaRespuestaAjax(respuesta) {
-    if (respuesta.length == 0)
-        $("#resultados").hide();
+function hacerPeticionAjax() {
+    busqueda = $("#barra_busqueda_principal").val();
+    
+    $.ajax({
+    data: {busqueda:busqueda},
+    url: 'ajax.php',
+    method: 'POST',
+    success: procesaRespuestaAjax
+    });
+}
 
-    /*else{
-        $("#resultados").show();
-        res = "";
-        respuesta.forEach(element => {
-            res += (element + "<br>");
-        });
-        $("#resultados").html(respuesta);
-    }*/
+function procesaRespuestaAjax(respuesta) {   
 
-    else{
-        $("#resultados").show();
-        $("#resultados").html(respuesta);
-    }
-  }
-  
+$("#resultados").html(respuesta);
+if ($("#resultados").text().trim().length > 0 )
+    $("#resultados").show();
+
+else
+    $("#resultados").hide();
+}
+
+function mostrarBusqueda(){
+    $("#barra_busqueda_principal").toggle();
+    $("#resultados").toggle();
+}
